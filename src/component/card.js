47b6e3805c8cc2b71db71da6c341cdbe82c46c94
milad <miladarabali1@gmail.com>
2023-03-16 23:08:54 +0330

    add product number && event for input

--- src/component/card.js
+++ src/component/card.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react'
 import style from "./card.module.css"
-
+import down from "../images/down.svg"
+import up from "../images/up.svg"
 
 export default class card extends Component {
+  constructor (){
+    super();
+    this.state = ({
+        count:0,
+    })
+  }
+  downhandler = () => {
+    if(this.state.count >= 1){
+  
+      this.setState(pervState =>({
+        count: pervState.count-1,
+  
+      }))
+    }
+  }
+      
+
+  uphandler = () => {
+    this.setState(pervState =>({
+       count: pervState.count+1,
+    }))
+
+  }
   render() {
     const {image ,name ,cost} = this.props;
+    const {count} = this.state;
     return (
-      
       <div className={style.container}>
         <img src={image} alt="bb"/>
         <h3>{name}</h3>
         <p>{cost}</p>
+        <div className={style.count}> 
+          <img className={this.state.count ? "" : style.deactive}  src={down} alt='f' onClick={this.downhandler}/>
+          <span>{count}</span>
+          <img className={style.countimage} src={up} alt='f' onClick={this.uphandler}/>
+        </div>
       </div>
     )
   }
--- src/component/search.js
+++ src/component/search.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import style from "./search.module.css"
 
 export default class search extends Component {
+  constructor(){
+    super();
+    this.state =  {
+      text:"",
+    }    
+  }
+  changetext = (event)=>{
+      this.setState({
+        text: event.target.value,
+
+
+
+      })
+  }
   render() {
     return (
       <div className={style.container}>
         <p>search what do want</p>
-        <input placeholder='search...'/>
-
+        <input type="text" value={this.state.text} onChange={this.changetext} placeholder='search...'/>
+        <span>{this.state.text}</span>
 
 
 
